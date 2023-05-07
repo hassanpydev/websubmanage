41b14e862e6a54ec887c3aef79422769fe7dd905
hassandevepy <halsmiri@ucas.edu.ps>
2023-05-07 19:01:23 +0300

    add search util

--- submanage/google_util/chate_gpt_util.py
+++ submanage/google_util/chate_gpt_util.py
@@ -1,4 +1,4 @@
-API_KEY = "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"
+API_KEY = "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"
 # Note: you need to be using OpenAI Python v0.27.0 for the code below to work
 import openai
 
